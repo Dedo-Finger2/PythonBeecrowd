e85f264c1521c86eabd616df935165ab760bb524
Antonio Mauricio <127690696+Dedo-Finger2@users.noreply.github.com>
2023-10-16 08:33:16 -0700

    modificada a descrição dos desafios

--- Desafios/py_1000.py
+++ Desafios/py_1000.py
@@ -1,16 +1,4 @@
-"""
-Descrição desafio 1000 - Hello World
-
-LINK: https://www.beecrowd.com.br/judge/pt/problems/view/1000
-
-O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema
-tudo o que você precisa fazer é imprimir esta mensagem na tela.
-
-Entrada
-Este problema não possui nenhuma entrada.
-
-Saída
-Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
-"""
+# Hello World
+# LINK: https://www.beecrowd.com.br/judge/pt/problems/view/1000
 
 print("Hello World!")
