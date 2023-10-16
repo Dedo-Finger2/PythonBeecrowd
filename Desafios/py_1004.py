0977b624d79c07ec9ac0e2f481321ccb337ee7f5
Antonio Mauricio <127690696+Dedo-Finger2@users.noreply.github.com>
2023-10-16 09:04:22 -0700

    desafio 1004 feito

---
+++ Desafios/py_1004.py
@@ -0,0 +1,9 @@
+# Produto simples
+# LINK: https://www.beecrowd.com.br/judge/pt/problems/view/1004
+
+value_1: int = int(input())
+value_2: int = int(input())
+
+prod: int = value_1 * value_2
+
+print(f"PROD = {prod}")
