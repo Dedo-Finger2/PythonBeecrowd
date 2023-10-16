12d06558dd1ed28d4bb07d98e73b25df0fdc9114
Antonio Mauricio <127690696+Dedo-Finger2@users.noreply.github.com>
2023-10-16 08:39:53 -0700

    desafio 1001 feito

---
+++ Desafios/py_1001.py
@@ -0,0 +1,9 @@
+# Extremamente Básico
+# LINK: https://www.beecrowd.com.br/judge/pt/problems/view/1001
+
+a: int = int(input())
+b: int = int(input())
+
+soma: int = a + b
+
+print(f"X = {str(soma)}")
